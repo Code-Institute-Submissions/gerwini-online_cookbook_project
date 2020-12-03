c131c8815b7c55ca573c573fd5553d2bb1cef08d
gerwini <gerwinjb@hotmail.nl>
2020-12-03 18:36:51 +0000

    wired the task button to take to template

--- app.py
+++ app.py
@@ -116,6 +116,14 @@ def add_recipe():
     return render_template("add_recipe.html", meals=meals)
 
 
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+
+    meals = mongo.db.meals.find().sort("meal_type", 1)
+    return render_template("edit_recipe.html", recipe=recipe, meals=meals)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
