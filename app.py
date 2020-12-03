3425b941bc26e7e7543ab37a84e4eae597a94750
gerwini <gerwinjb@hotmail.nl>
2020-12-03 20:33:49 +0000

    search functionality added

--- app.py
+++ app.py
@@ -25,6 +25,19 @@ def get_recipes():
     return render_template("recipes.html", recipes=recipes)
 
 
+@app.route("/search_recipe")
+def search_recipe():
+    recipes = list(mongo.db.recipes.find())
+    return render_template("search_recipe.html", recipes=recipes)
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
